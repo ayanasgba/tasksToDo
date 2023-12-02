77e2b8dbc28e9cb150c020b4fbb3d697a5ed9f73
Aiana Sagynbek <ayana12q34@gmail.com>
2023-12-02 17:44:53 +0600

    init commit

--- src/main/java/com/example/taskToDo/controller/TaskController.java
+++ src/main/java/com/example/taskToDo/controller/TaskController.java
@@ -7,8 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("api/tasks")
 public class TaskController {
